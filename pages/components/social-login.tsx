f709963e4f38ef3c7109504ca456bf91d6e98d1b
kingdanie <daniedemola@gmail.com>
2022-11-12 13:17:15 +0100

    add social login component

---
+++ pages/components/social-login.tsx
@@ -0,0 +1,30 @@
+export default function SocialLogin() {
+    return (
+        <div className="flex justify-between">
+        <div className="flex flex-col gap-y-2 items-center rounded-lg  border-gray-300 bg-white" >
+            <div className="flex p-5 shadow rounded-lg border">
+                <img src="" alt="" className="w-6 h-6" />
+            </div>
+            <p className="text-xs text-gray-500">Google</p>
+        </div>
+        <div className="flex flex-col gap-y-2 items-center rounded-lg  border-gray-300 bg-white" >
+            <div className="flex p-5 shadow rounded-lg border">
+                <img src="" alt="" className="w-6 h-6" />
+            </div>
+            <p className="text-xs text-gray-500">Facebook</p>
+        </div>
+        <div className="flex flex-col gap-y-2 items-center rounded-lg  border-gray-300 bg-white" >
+            <div className="flex p-5 shadow rounded-lg border">
+                <img src="" alt="" className="w-6 h-6" />
+            </div>
+            <p className="text-xs text-gray-500">Apple</p>
+        </div>
+        <div className="flex flex-col gap-y-2 items-center rounded-lg  border-gray-300 bg-white" >
+            <div className="flex p-5 shadow rounded-lg border">
+                <img src="" alt="" className="w-6 h-6" />
+            </div>
+            <p className="text-xs text-gray-500">Microsoft</p>
+        </div>
+    </div>
+    )
+}
