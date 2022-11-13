db5177fb532e8358b8af71a765493a00bb3ce325
kingdanie <daniedemola@gmail.com>
2022-11-13 23:46:15 +0100

    add history page

---
+++ pages/history.tsx
@@ -0,0 +1,12 @@
+import RootLayout from "./layout"
+import OtherLayout from "./layout2"
+
+export default function History() {
+    return (
+        <OtherLayout>
+    
+            <h1>History</h1>
+    
+        </OtherLayout>
+    )
+}
