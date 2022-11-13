5aff766d5f055dbdea5c5bb6bf995475bfb009b2
kingdanie <daniedemola@gmail.com>
2022-11-13 23:45:39 +0100

    add settings page

---
+++ pages/settings.tsx
@@ -0,0 +1,10 @@
+import OtherLayout from "./layout2";
+
+export default function Settings() {
+    return(
+        <OtherLayout>
+
+            <h1>Settings Page</h1>
+        </OtherLayout>
+    )
+}
