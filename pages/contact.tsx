64ba8360078c1aa668b2852cab1805f555af2286
kingdanie <daniedemola@gmail.com>
2022-11-13 23:46:32 +0100

    add contact pages

---
+++ pages/contact.tsx
@@ -0,0 +1,15 @@
+import OtherLayout from "./layout2"
+export default function About() {
+    return (
+
+        
+        <OtherLayout>
+            <div>
+
+              <h2>Come on</h2>
+              <p>Waht is happening here, I dont understand why you are not being centered</p>
+            </div>
+        </OtherLayout>
+       
+    )
+}
