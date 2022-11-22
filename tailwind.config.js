617a8169cafdbd5ab0182eaebc9ac1cc02a3c25f
kingdanie <daniedemola@gmail.com>
2022-11-22 02:08:10 +0100

    adding brand colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,26 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
+    extend: {
+     colors: { 
+      transparent: 'transparent',
+      current: 'currentColor',
+      primary: 'rgb(var(--color-primary) / <alpha-value>)',
+      accent: 'rgba(var(--color-accent) / <alpha-value>)',
+      success: 'rbg(var(--color-sucess)  / <alpha-value>)',
+      info: 'rbg(var(--color-info) / <alpha-value>)',
+      warning: 'rbg(var(--color-warning) / <alpha-value>)',
+      danger: 'rbg(var(--color-danger) / <alpha-value>)',
+      purpledark: 'rbg(var(--color-dark-purple) / <alpha-value>)',
+      text: 'rbg(var(--color-text-color) / <alpha-value>)',
+      searchBar: 'rbg(var(--color-search-bar) / <alpha-value>)',
+      hover: 'rbg(var(--color-hover) / <alpha-value>)',
+      background: 'rbg(var(--color-background) / <alpha-value>)',
+      filler: '#632887',
+      text2: 'rgb(var(--color-text-2) / <alpha-value>)',
+      mainBg: 'rgb(var(--color-main-bg) / <alpha-value>)',
+      }
+    }
+    },
   plugins: [],
 }
