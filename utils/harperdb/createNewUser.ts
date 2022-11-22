ae5f39ba6acfb7f56c2b4e3ee9a888357ac5e4fe
kingdanie <daniedemola@gmail.com>
2022-11-22 23:27:30 +0100

    signup api functions

---
+++ utils/harperdb/createNewUser.ts
@@ -0,0 +1,34 @@
+// This function can only be ran on the backend as it requires a "super_user" password
+export const harperCreateNewUser = async (
+    email: string,
+    username: string,
+    password: string
+  ) => {
+    const DB_URL = process.env.DB_URL
+    const DB_PW = process.env.HARPERDB_PW
+    if (!DB_URL || !DB_PW) {
+      console.log("Error: .env variables are undefined")
+      throw "Internal server error"
+    }
+    const myHeaders = new Headers()
+    myHeaders.append("Content-Type", "application/json")
+    myHeaders.append("Authorization", DB_PW)
+    const raw = JSON.stringify({
+      operation: "add_user",
+      role: "standard_user",
+      email: email.toLowerCase(),
+      username: username.toLowerCase(),
+      password: password,
+      active: true,
+    })
+    const requestOptions: RequestInit = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    }
+  
+    const response = await fetch(DB_URL, requestOptions)
+    const result = await response.json()
+    return { response, result }
+  }
