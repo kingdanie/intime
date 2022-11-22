4f8e5986bd0f5f0526f2a344f5e96859c2611906
kingdanie <daniedemola@gmail.com>
2022-11-22 02:09:08 +0100

    change colors to brand guide

--- pages/components/PointCard.tsx
+++ pages/components/PointCard.tsx
@@ -4,7 +4,7 @@ export default function PointCard({showModal}) {
   return (
     <>
       <div className="flex text-white items-center h-40 w-80 relative">
-        <div className="flex text-white items-center rounded-xl p-5 shadow-lg h-40 w-11/12 bg-purple-800 bg-card">
+        <div className="flex text-white items-center rounded-xl p-5 shadow-lg h-40 w-11/12 bg-filler bg-card">
           <div className="">
             <div className="border-white border-2 px-1 rounded-sm text-white mr-7">
               co
@@ -13,7 +13,7 @@ export default function PointCard({showModal}) {
           <div className="flex flex-col space-y-1">
             <p className="text-sm text-white">Points</p>
             <div className="text-4xl font-bold">243</div>
-            <div className="text-yellow-300 text-sm">N 2304</div>
+            <div className="text-accent text-sm">N 2304</div>
           </div>
         </div>
         <button onClick={()=> showModal()} className="text-sm text-gray-600 font-semibold absolute right-0 flex py-4 px-7 rounded-xl bg-yellow-400">
