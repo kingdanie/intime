1d09e0ee3879a547d8d6ac45dd54d8b633a744e4
kingdanie <daniedemola@gmail.com>
2022-11-26 00:08:37 +0100

    remove unused const from helper func

--- utils/harperdb/createNewUser.ts
+++ utils/harperdb/createNewUser.ts
@@ -1,10 +1,9 @@
 // This function can only be ran on the backend as it requires a "super_user" password
+import { DB_URL } from "../../constants/constants"
 export const harperCreateNewUser = async (
-    email: string,
     username: string,
     password: string
   ) => {
-    const DB_URL = process.env.DB_URL
     const DB_PW = process.env.HARPERDB_PW
     if (!DB_URL || !DB_PW) {
       console.log("Error: .env variables are undefined")
@@ -16,7 +15,6 @@ export const harperCreateNewUser = async (
     const raw = JSON.stringify({
       operation: "add_user",
       role: "standard_user",
-      email: email.toLowerCase(),
       username: username.toLowerCase(),
       password: password,
       active: true,
