10dff2990c84c51c261934c954a2cbc1a089f1f3
kingdanie <daniedemola@gmail.com>
2022-11-26 00:04:39 +0100

    wrap app shell with user context provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { UserContext } from '../contexts/UserContents'
+import { useUser } from '../custom-hooks/useUser'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const {username, setUsername, userInfo, setUserInfo} = useUser()
+  return (
+    <UserContext.Provider value={{ username, setUsername, userInfo, setUserInfo }}>
+<Component {...pageProps} />
+    </UserContext.Provider>
+  )
 }
