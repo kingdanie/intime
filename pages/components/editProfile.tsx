bb18fed02a14b77f60d220d5fc7d74a39e273e01
kingdanie <daniedemola@gmail.com>
2022-11-26 00:07:09 +0100

    dynamic data on edit profile comp

--- pages/components/editProfile.tsx
+++ pages/components/editProfile.tsx
@@ -3,16 +3,21 @@ import Image from "next/image";
 import Link from "next/link";
 import uimage from "./../../public/user-profile.png";
 import editButton from "./../../public/edit.svg";
+import { useContext } from "react";
+import { UserContext } from "../../contexts/UserContents";
 
 export default function EditProfile() {
+
+  const { username, userInfo } = useContext(UserContext)
+  
   return (
     <div className="w-full text-gray-600 flex flex-col items-center justify-between space-y-5 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-lg">
       <Image src={uimage} height="50" width="70" alt="user profile" />
       <div className="flex flex-col items-center">
-        <h3 className="font-extrabold mb-1">User Name</h3>
+        <h3 className="font-extrabold mb-1">{username}</h3>
         <div className="flex items-center">
           <EnvelopeIcon className="h-5 mr-2" />
-          <small>email@example.com</small>
+          <small>{userInfo[0]?.email}</small>
         </div>
       </div>
       <div>
