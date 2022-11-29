cf15e8cd840cb0aee421a2ad21b7e235f7a92302
kingdanie <daniedemola@gmail.com>
2022-11-29 19:53:50 +0100

    add type to user context

--- contexts/UserContents.ts
+++ contexts/UserContents.ts
@@ -1,8 +1,13 @@
+import { UserInfo } from './../types/UserInfo';
 import { createContext } from "react"
+
 
 export const UserContext = createContext({
   username: "",
   setUsername: (username: string) => {},
   userInfo: [],
-  setUserInfo: (userInfo: any) => {},
-})
+  setUserInfo: (userInfo: UserInfo) => {},
+} as { username: string,
+setUsername: React.Dispatch<React.SetStateAction<string>>,
+userInfo: UserInfo[],
+setUserInfo: React.Dispatch<React.SetStateAction<UserInfo>>})
