bf0418ea6c3f1ea8f58c4110d5eec6d291910b28
kingdanie <daniedemola@gmail.com>
2022-11-29 20:58:01 +0100

    Review message form

--- pages/messages.tsx
+++ pages/messages.tsx
@@ -1,40 +1,70 @@
 import { PaperAirplaneIcon, UserIcon } from "@heroicons/react/24/outline";
-import { FormEvent, useState } from "react";
+import { FormEvent, useContext, useState } from "react";
 import AppHeader from "../components/AppHeader";
 import MCalendar from "../components/m-calendar";
 import RecentPicks from "../components/RecentPicks";
 import AppSwitch from "../components/switch";
+import { UserContext } from "../contexts/UserContents";
 import OtherLayout from "./layout2";
+import { postFormData } from "../utils/postFormData";
+import { Message, Recipient } from "../types/Message";
+
 export default function Messages() {
-  
-      // const contactList: string[] = []
-      const [senderId, setSenderId] = useState('');
-      const [contact, setContact] = useState('');
-      const [msg, setMsg] = useState('');
-      const [contactList, setContactList] = useState([] as string[])
-      // const addContact = () => {
-      //   setContactList([ ...contactList, contact])
-      // }
+  const { username, userInfo } = useContext(UserContext);
 
-      interface Contact {
-        id?: number,
-        name: string,
-        phone: number,
-        email?: string
-      }
+  // const contactList: string[] = []
+  const [senderId, setSenderId] = useState("");
+  const [contact, setContact] = useState("");
+  const [msg, setMsg] = useState("");
+  const [contactList, setContactList] = useState([] as Recipient[]);
+  // const addContact = () => {
+  //   setContactList([ ...contactList, contact])
+  // }
 
-      const handleSubmit = (e: FormEvent) => {
-        e.preventDefault()
-        let message = {
-          senderId: senderId,
-          contact: contact,
-          contactList: contactList,
-          message: msg
-        }
-        console.log(message)
-      }
+  const addContact = () => {
+    setContactList([...contactList, {name: contact, phone: senderId}])
+    setSenderId("")
+    setContact("")
+  }
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // let setDay = new Date().getTime()
+    let message: Message = {
+      reciever_number: contactList,
+      message: msg,
+      username: username,
+      sent_date: `${new Date().getTime()}`
+    };
+    console.log(message);
+
+    if (contactList.length == 0 && senderId == "") return;
+
+    const { response, result } = await postFormData(message, "/api/messages");
+
+    // Account not created successfully
+    if (response.status !== 200) {
+      // setErrors(result.error);
+      alert(result.error);
+      return;
+    }
+
+    if (response.status == 200) {
+      alert("sucesfully added");
+
+      console.log({ response, result });
+
+      //reset form
+      setContact("");
+      setMsg("");
+      setContactList([]);
+      setSenderId("");
+    } else {
+      alert(result.error);
+    }
+  };
   return (
-
     <OtherLayout>
       <div
         aria-labelledby="primary-heading"
@@ -49,7 +79,10 @@ export default function Messages() {
           </div>
         </section>
         <section className="py-14">
-          <form onSubmit={handleSubmit} className="flex flex-col space-y-5 justify-start">
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col space-y-5 justify-start"
+          >
             <div className="relative rounded-md shadow-sm">
               <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                 <UserIcon
@@ -87,8 +120,14 @@ export default function Messages() {
                 value={contact}
                 onChange={(e) => setContact(e.target.value.trim())}
               />
-              <div onClick={() =>  setContactList([ ...contactList, contact])} className="absolute items-center inset-y-0 right-0 flex py-1.5 pr-1.5">
-                <button type="button" className="inline-flex h-7 w-2 justify-center items-center rounded-xl border bg-accent border-gray-200 p-4 font-sans text-sm font-medium text-gray-400">
+              <div
+                onClick={addContact}
+                className="absolute items-center inset-y-0 right-0 flex py-1.5 pr-1.5"
+              >
+                <button
+                  type="button"
+                  className="inline-flex h-7 w-2 justify-center items-center rounded-xl border bg-accent border-gray-200 p-4 font-sans text-sm font-medium text-gray-400"
+                >
                   +
                 </button>
               </div>
@@ -118,36 +157,31 @@ export default function Messages() {
             </div> */}
 
             <div className="grid grid-cols-4 gap-3">
-
-            {contactList?.map((icontact, i) => (
-              
-          <div key={i} className="px-4 py-2 shadow-sm rounded-lg border-solid border-gray-300 border-2 space-x-5 flex justify-between">
-            
-
-            <div className="flex items-center space-x-2">
-
-
-            <div
-                    className="rounded-full border-solid border-2 border-gray-300 py-1 px-2 hover:bg-gray-300 
+              {contactList?.map((icontact, i) => (
+                <div
+                  key={i}
+                  className="px-4 py-2 shadow-sm rounded-lg border-solid border-gray-300 border-2 space-x-5 flex justify-between"
+                >
+                  <div className="flex items-center space-x-2">
+                    <div
+                      className="rounded-full border-solid border-2 border-gray-300 py-1 px-2 hover:bg-gray-300 
                     flex items-center justify-center
                     text-xs
                     "
-                  >
-                   
-                    {"hello".toUpperCase().slice(0, 1)}
+                    >
+                      {"hello".toUpperCase().slice(0, 1)}
+                    </div>
+                    <small>{icontact.name ? icontact.name : icontact.phone}</small>
                   </div>
-            <small>{icontact}</small>
-            </div>
-            <button onClick={() => {
-              setContactList(
-                contactList.filter(a =>
-                  a !== icontact
-                )
-              );
-            }}>x</button>
-            
-            </div>
-        ))}
+                  <button
+                    onClick={() => {
+                      setContactList(contactList.filter((a) => a !== icontact));
+                    }}
+                  >
+                    x
+                  </button>
+                </div>
+              ))}
             </div>
             <div>
               <textarea
