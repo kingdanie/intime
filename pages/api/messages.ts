8d31c943d043b1e461ff53ecff5e86d17bb89955
kingdanie <daniedemola@gmail.com>
2022-11-29 20:57:04 +0100

    reviewed mesage obj of message api

--- pages/api/messages.ts
+++ pages/api/messages.ts
@@ -1,54 +1,52 @@
-import type { NextApiRequest, NextApiResponse } from "next"
-import { harperCreateMsg } from "../../utils/harperdb/createMsg"
-import handler from "./../../middleware/_defaultHandler"
-
-
+import type { NextApiRequest, NextApiResponse } from "next";
+import { harperCreateMsg } from "../../utils/harperdb/createMsg";
+import handler from "./../../middleware/_defaultHandler";
 
 export default handler.post(
   async (req: NextApiRequest, res: NextApiResponse) => {
-    const { senderId, contactList, message, username, email } = req.body
+    const { reciever_number, message, username, sent_date } = req.body;
 
-    const errors: string[] = getFormErrors(senderId, contactList, message, username, email)
+    const errors: string[] = getFormErrors(
+      reciever_number,
+      message,
+      username,
+      sent_date,
+    );
     if (errors.length > 0) {
-      return res.status(400).json({ error: errors })
+      return res.status(400).json({ error: errors });
     }
 
-     // Create new user and userinfo with HarperDB, and send back result
-     try {
-
-
-        const { response, result } = await harperCreateMsg(
-                senderId,
-                contactList,
-                message,
-                username, 
-                email
-        )        
-        return  res.status(response.status).json(result)
-
-      } catch (err) {
-        console.log(err)
-        return res.status(500).json({ error: err })
-      }
+    // Save new message into HarperDB, and send back result
+    try {
+      const { response, result } = await harperCreateMsg(
+        reciever_number,
+      message,
+      username,
+      sent_date,
+      );
+      return res.status(response.status).json(result);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ error: err });
+    }
   }
-)
+);
 
 const getFormErrors = (
-    senderId: string,
-  contactList: string[],
-  message: string,
-  username: string, 
-  email: string
+    reciever_number: [],
+    message: string,
+    username: string,
+    sent_date: string,
 ) => {
-  const errors: string[] = []
-  if (!senderId || !contactList || !message) {
-    errors.push("All fields are required")
+  const errors: string[] = [];
+  if (reciever_number.length == 0 || !message) {
+    errors.push("All fields are required");
   }
-  if (contactList.length < 1) {
-    errors.push("Please choose atleasst one recipient")
+  if (reciever_number.length < 1) {
+    errors.push("Please choose atleasst one recipient");
   }
-  if (message.length < 4 ) {
-    errors.push("Message must be a minimum of 4 characters")
+  if (message.length < 4) {
+    errors.push("Message must be a minimum of 4 characters");
   }
-  return errors
-}
+  return errors;
+};
