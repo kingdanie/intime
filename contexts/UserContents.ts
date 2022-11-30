ea5abe4452462698d5ea1ad71e00c513fa9ba009
kingdanie <daniedemola@gmail.com>
2022-11-30 23:56:19 +0100

    revise UserContexts interface

--- contexts/UserContents.ts
+++ contexts/UserContents.ts
@@ -2,12 +2,16 @@ import { UserInfo } from './../types/UserInfo';
 import { createContext } from "react"
 
 
+interface UserContext { 
+  username: string,
+  setUsername: React.Dispatch<React.SetStateAction<string>>,
+  userInfo: UserInfo,
+  setUserInfo: React.Dispatch<React.SetStateAction<UserInfo>>
+}
+
 export const UserContext = createContext({
   username: "",
   setUsername: (username: string) => {},
   userInfo: {},
   setUserInfo: (userInfo: UserInfo) => {},
-} as { username: string,
-setUsername: React.Dispatch<React.SetStateAction<string>>,
-userInfo: UserInfo,
-setUserInfo: React.Dispatch<React.SetStateAction<UserInfo>>})
+} as UserContext )
