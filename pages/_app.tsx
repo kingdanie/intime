444fbc236900943b7682c2a5660ee9ce073d7180
kingdanie <daniedemola@gmail.com>
2022-11-30 18:00:16 +0100

    feat: add contact context provider to shell

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { UserContext } from '../contexts/UserContents'
-import { useUser } from '../custom-hooks/useUser'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { UserContext } from "../contexts/UserContents";
+import { useUser } from "../custom-hooks/useUser";
+import { ContactsContext } from "../contexts/ContactsContext";
+import { useContacts } from "../custom-hooks/useContact";
 
-export default function App({ Component, pageProps }: AppProps) {
-  const {username, setUsername, userInfo, setUserInfo} = useUser()
+export default function App({
+  Component,
+  pageProps,
+}: AppProps) {
+  const { username, setUsername, userInfo, setUserInfo } =
+    useUser();
+  const { contacts, setContacts, getAndSetContacts } =
+    useContacts(username);
   return (
-    <UserContext.Provider value={{ username, setUsername, userInfo, setUserInfo }}>
-<Component {...pageProps} />
+    <UserContext.Provider
+      value={{
+        username,
+        setUsername,
+        userInfo,
+        setUserInfo,
+      }}
+    >
+      <ContactsContext.Provider
+        value={{ contacts, setContacts, getAndSetContacts }}
+      >
+        <Component {...pageProps} />
+      </ContactsContext.Provider>
     </UserContext.Provider>
-  )
+  );
 }
