6672b49de9857a2b7f9ef45c9634b7deeeb3edb8
kingdanie <daniedemola@gmail.com>
2022-11-30 17:58:56 +0100

    feat: change state colors of switch

--- components/switch.tsx
+++ components/switch.tsx
@@ -9,7 +9,7 @@ export default function AppSwitch({ name }) {
       <Switch
         checked={enabled}
         onChange={setEnabled}
-        className={`${enabled ? "bg-teal-900" : "bg-teal-700"}
+        className={`${enabled ? "bg-filler" : "bg-slate-200"}
           relative inline-flex h-[25px] w-[40px] items-center px-1 shrink-0 cursor-pointer rounded-full border-2 border-transparent 
           transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  
           focus-visible:ring-white focus-visible:ring-opacity-75`}
@@ -17,8 +17,8 @@ export default function AppSwitch({ name }) {
         <span className="sr-only">{name}</span>
         <span
           aria-hidden="true"
-          className={`${enabled ? "translate-x-4" : "translate-x-0"}
-            pointer-events-none inline-block h-[12px] w-[12px] transform rounded-full bg-white 
+          className={`${enabled ? "translate-x-4 bg-white" : "translate-x-0 bg-slate-400"}
+            pointer-events-none inline-block h-[12px] w-[12px] transform rounded-full 
             shadow-lg ring-0 transition duration-200 ease-in-out`}
         />
       </Switch>
