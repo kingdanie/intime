3c72edc9db4468a805978371b99c14d453e3547a
kingdanie <daniedemola@gmail.com>
2022-11-30 18:03:50 +0100

    feat: create group contact help func

---
+++ utils/harperdb/createContactGroup.ts
@@ -0,0 +1,38 @@
+import { DB_URL } from "../../constants/constants";
+
+export const harperCreateContactGroup = async (
+  username: string,
+  name: string,
+  color?: string
+) => {
+  const DB_PW = process.env.HARPERDB_PW;
+  if (!DB_URL || !DB_PW) {
+    console.log("Error: .env variables are undefined");
+    throw "Internal server error";
+  }
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("Authorization", DB_PW);
+  const raw = JSON.stringify({
+    operation: "insert",
+    schema: "messages",
+    table: "contact_groups",
+    records: [
+      {
+        name: name,
+        username: username,
+        color: color,
+      },
+    ],
+  });
+  const requestOptions: RequestInit = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  const response = await fetch(DB_URL, requestOptions);
+  const result = await response.json();
+  return { response, result };
+};
