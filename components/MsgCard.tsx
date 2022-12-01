63ea8ad3b1dce7bedb2af909257cad2ca982c9cb
kingdanie <daniedemola@gmail.com>
2022-12-01 14:53:17 +0100

    css: improve color hover state of comp

--- components/MsgCard.tsx
+++ components/MsgCard.tsx
@@ -1,7 +1,14 @@
-export default function MsgCard({ message }) {
+import { Message } from "../types/Message";
+
+export default function MsgCard({ message }: {message: Message}) {
   return (
     <>
-      <div className="relative flex my-3 items-center justify-between space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-pink-500 focus-within:ring-offset-2 hover:border-gray-400">
+      <div className="relative flex my-3 items-center justify-between space-x-3 
+              rounded-lg border border-brdcolor bg-white px-6 py-5 
+              shadow focus-within:border-gray-100 focus-within:ring-2 focus-within:ring-filler
+              focus-within:ring-offset-2 hover:border-gray-300
+            "
+      >
         <div className="flex space-x-3">
           <div className="flex-shrink-0">
             <img className="h-10 w-10 rounded-full" src="/mary.png" alt="" />
@@ -9,7 +16,7 @@ export default function MsgCard({ message }) {
           <div className="min-w-0 flex-1">
             <a href="#" className="focus:outline-none">
               <span className="absolute inset-0" aria-hidden="true" />
-              <p className="text-sm font-medium text-gray-900">{message.recipient.name}</p>
+              <p className="text-sm font-medium text-gray-900">{message.recipient?.name}</p>
               <p className="truncate text-sm text-gray-500">{message.message}</p>
               <div className="bg-gray-600 rounded-md text-white text-xs text-center ">
                 <span></span>
